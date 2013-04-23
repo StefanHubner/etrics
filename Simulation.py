7a94447266ced4a3930595633c861094bb0ba0c2
Stefan Hubner <s.hubner@tilburguniversity.edu>
2013-04-23 11:34:11 +0200

    AddStatistics function

--- Simulation.py
+++ Simulation.py
@@ -10,14 +10,20 @@ class Simulation:
 	__parameters = []
 	__estimates = []
 	__samplesize = []
+	__evaluations = {Results.Bias:{}, Results.Original:{}} 
 
 	def __init__(self):
 		self.Generating = EventHook(maxhandlers=1)
 		self.Estimating = EventHook(maxhandlers=1)
 		self.PostEstimation = EventHook()
+
+		self.AddStatistics({"Average":mean, "Variance":var}, type=Results.Original)
+		self.AddStatistics({"Bias":mean}, type=Results.Bias)
+		self.AddStatistics({"RMSE": lambda x,axis: sqrt(power(mean(x, axis=axis), 2)+var(x, axis=axis))})
 	
-	def SetParameters(self, pars):
+	def SetParameters(self, pars, names):
 		self.__parameters = pars
+		self.__parnames = names
 
 	def SetSampleSize(self, N):
 		self.__samplesize = N
@@ -27,13 +33,16 @@ class Simulation:
 			self.__data = self.Generating.Fire(self.__samplesize, self.__parameters)[0]
 			self.__estimates.append(self.Estimating.Fire(self.__data)[0])
 	
-	def GetResults(self, delegates, type=Results.Bias):
-		return [ (title, delegates[title](matrix([coef -multiply(self.__parameters, double(type==Results.Bias)) \
-			for coef in self.__estimates]), 0)) for title in delegates]
-			
-	def PrintTable(self, results, coefnames):
-		print "{0}{1}".format(" "*21, "".join(["{: >10}".format(cname) for cname in coefnames]))
-		for k,v in res1+res2:
+	def AddStatistics(self, delegates, type=Results.Bias):
+		for k,v in delegates.iteritems(): 
+			(self.__evaluations[type])[k] = v
+
+	def GetResults(self, type):
+		return [ (title, fct(matrix([coef -multiply(self.__parameters, double(type==Results.Bias)) for coef in self.__estimates]), 0)) for title,fct in self.__evaluations[type].iteritems()] 
+
+	def PrintTable(self):
+		print "{0}{1}".format(" "*21, "".join(["{: >10}".format(cname) for cname in self.__parnames]))
+		for k,v in self.GetResults(Results.Original)+self.GetResults(Results.Bias):
 			print "{0: <20} {1}".format(k, "".join(["{:10.4f}".format(a) for a in (asarray(v).tolist())[0]]))
 			
 
@@ -49,14 +58,16 @@ def estimateModel(data):
 	return res.params
 
 x = Simulation()
-x.SetParameters([1,2,3])
+x.SetParameters([1,2,3], ("beta{} "*3).format(1,2,3).split())
 x.SetSampleSize(1000)
 x.Generating += createData
 x.Estimating += estimateModel
 x.Simulate(100)
-res1 = x.GetResults( {"Bias":mean, "RMSE": lambda x,axis: sqrt(power(mean(x, axis=axis), 2)+var(x, axis=axis))}, \
-	type=Results.Bias )
-res2 = x.GetResults( {"Average":mean, "Variance":var, \
-	"95th Quantile": lambda x,axis: [scoreatpercentile(x,95, axis=axis)]}, type=Results.Original )
 
-x.PrintTable(res1+res2, ("beta{} "*3).format(1,2,3).split())
+x.AddStatistics({"95th Quantile": lambda x,axis: [scoreatpercentile(x, 95, axis=axis)]}, type=Results.Original)
+x.AddStatistics({"Median": lambda x,axis: [scoreatpercentile(x, 50, axis=axis)]}, type=Results.Original)
+
+res1 = x.GetResults(Results.Original)
+res2 = x.GetResults(Results.Bias)
+
+x.PrintTable()
